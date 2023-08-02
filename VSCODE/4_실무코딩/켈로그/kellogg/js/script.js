d2813d7bb18864b67e81c992259762ec9d9a1253
JaeYoungYoon <dvlen486@gmail.com>
2023-08-02 15:58:08 +0900

    script 부분 추가

--- VSCODE/4_실무코딩/켈로그/kellogg/js/script.js
+++ VSCODE/4_실무코딩/켈로그/kellogg/js/script.js
@@ -1,33 +1,23 @@
 $(document).ready(function () {
-    // alert('확인');
-
-    /*
-    
-    원래는 header에 active 라는 클래스 X
-    화살표는 아래 모양
-    서브메뉴 안보임
-
-    메인메뉴에 hover하면 서브메뉴가 보이고
-    header에 active 클래스 추가
-    서브메뉴가 보임 (transition을 여기에 준다)
-    화살표는 위 모양
-    
-    */
 
     $(".gnb > ul > li:first-child").mouseover(function(){
         $("#header").addClass("active");
         $("#header").css("height","600px");
+        $(".sub").css("display","block");
     }).mouseout(function(){
         $("#header").removeClass("active");
         $("#header").css("height","184px");
+        $(".sub").css("display","none");
     });
 
     $(".gnb > ul > li:last-child").mouseover(function(){
         $("#header").addClass("active");
         $("#header").css("height","600px");
+        $(".sub").css("display","block");
     }).mouseout(function(){
         $("#header").removeClass("active");
         $("#header").css("height","184px");
+        $(".sub").css("display","none");
     });
 
 });
