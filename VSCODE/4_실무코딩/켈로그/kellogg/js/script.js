c095f104f43a2bebd72dcfee4007c780948ec747
jyma47 <102508927+jyma47@users.noreply.github.com>
2023-08-02 12:24:43 +0900

    Add files via upload

---
+++ VSCODE/4_실무코딩/켈로그/kellogg/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    // alert('확인');
+
+    /*
+    
+    원래는 header에 active 라는 클래스 X
+    화살표는 아래 모양
+    서브메뉴 안보임
+
+    메인메뉴에 hover하면 서브메뉴가 보이고
+    header에 active 클래스 추가
+    서브메뉴가 보임
+    화살표는 위 모양
+    
+    */
+});
